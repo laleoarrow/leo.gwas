6ca304fc61d697d6b5ef92293275420cbcde1a2d
laleoarrow <luao@stu.cqmu.edu.cn>
2024-09-07 22:55:35 +0800

    Resolve merge conflicts

---
+++ R/0.Pre.R
@@ -0,0 +1,15 @@
+#' Title Get ID using CHR, BP, A2 (REF/Non-effect), A1 (ALT/Effect)
+#'
+#' @param x data.frame
+#'
+#' @return data.frame with ID column and the character counts of A1 and A2
+#' @export
+#'
+#' @examples
+get_id <- function(x) {
+  x <- x %>% 
+    mutate(ID = paste(CHR, BP, A2, A1, sep = ":"),
+           A1_n = nchar(A1),
+           A2_n = nchar(A2))
+  return(x)
+}
